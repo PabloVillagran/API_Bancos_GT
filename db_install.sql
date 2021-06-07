171e6b0b1a494ab0875c6f5b133c52ecd7fde9f0
Pablo Daniel Villagrán <pdvillc@gmail.com>
2021-06-06 21:46:09 -0600

    Correcciones a SQL autogenerado, +AUTO_INCREMENT en columnas llave, -VISIBLE en llaves foraneas.

--- db_install.sql
+++ db_install.sql
@@ -10,6 +10,19 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`detalle_cheque` ;
+DROP TABLE IF EXISTS `mydb`.`detalle_estado_cuenta` ;
+DROP TABLE IF EXISTS `mydb`.`Consulta_Cheque` ;
+DROP TABLE IF EXISTS `mydb`.`Consulta_saldo` ;
+DROP TABLE IF EXISTS `mydb`.`Consulta_estado_cuenta` ;
+DROP TABLE IF EXISTS `mydb`.`Pago_servicio_diverso` ;
+DROP TABLE IF EXISTS `mydb`.`Servicio_afiliado` ;
+DROP TABLE IF EXISTS `mydb`.`Transferencia` ;
+DROP TABLE IF EXISTS `mydb`.`Pago_servicio_banca` ;
+DROP TABLE IF EXISTS `mydb`.`Cuenta_asociada` ;
+DROP TABLE IF EXISTS `mydb`.`Sesion_usuario` ;
+DROP TABLE IF EXISTS `mydb`.`API_Banco` ;
+DROP TABLE IF EXISTS `mydb`.`Banco` ;
 
 -- -----------------------------------------------------
 -- Schema mydb
@@ -20,10 +33,8 @@ USE `mydb` ;
 -- -----------------------------------------------------
 -- Table `mydb`.`Banco`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Banco` ;
-
 CREATE TABLE IF NOT EXISTS `mydb`.`Banco` (
-  `idBanco` INT NOT NULL,
+  `idBanco` INT NOT NULL AUTO_INCREMENT,
   `Nombre` VARCHAR(45) NULL,
   PRIMARY KEY (`idBanco`))
 ENGINE = InnoDB;
@@ -32,10 +43,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`API_Banco`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`API_Banco` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`API_Banco` (
-  `idAPI` INT NOT NULL,
+  `idAPI` INT NOT NULL AUTO_INCREMENT,
   `endpoint` VARCHAR(45) NULL,
   `Banco_idBanco1` INT NOT NULL,
   PRIMARY KEY (`idAPI`),
@@ -46,16 +56,15 @@ CREATE TABLE IF NOT EXISTS `mydb`.`API_Banco` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_API_Banco_Banco1_idx` ON `mydb`.`API_Banco` (`Banco_idBanco1` ASC) VISIBLE;
+CREATE INDEX `fk_API_Banco_Banco1_idx` ON `mydb`.`API_Banco` (`Banco_idBanco1` ASC) ;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Sesion_usuario`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Sesion_usuario` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Sesion_usuario` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `Nombre_usuario` VARCHAR(45) NULL,
   `Token` VARCHAR(45) NULL,
   `fecha_hora_expira` VARCHAR(45) NULL,
@@ -69,16 +78,15 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Sesion_usuario` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_sesion_usuario_API_Banco1_idx` ON `mydb`.`Sesion_usuario` (`API_Banco_idAPI` ASC) VISIBLE;
+CREATE INDEX `fk_sesion_usuario_API_Banco1_idx` ON `mydb`.`Sesion_usuario` (`API_Banco_idAPI` ASC) ;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Cuenta_asociada`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Cuenta_asociada` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Cuenta_asociada` (
-  `numero_cuenta` INT NOT NULL,
+  `numero_cuenta` INT NOT NULL AUTO_INCREMENT,
   `sesion_usuario_id` INT NOT NULL,
   `isCredito` INT NULL,
   PRIMARY KEY (`numero_cuenta`, `sesion_usuario_id`),
@@ -89,16 +97,15 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Cuenta_asociada` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_cuenta_asociada_sesion_usuario1_idx` ON `mydb`.`Cuenta_asociada` (`sesion_usuario_id` ASC) VISIBLE;
+CREATE INDEX `fk_cuenta_asociada_sesion_usuario1_idx` ON `mydb`.`Cuenta_asociada` (`sesion_usuario_id` ASC) ;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Pago_servicio_banca`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Pago_servicio_banca` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Pago_servicio_banca` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `Monto` DOUBLE NULL,
   `Identificador_servicio` VARCHAR(45) NULL,
   `id_nota_credito` VARCHAR(45) NULL,
@@ -118,18 +125,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Pago_servicio_banca` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Pago_servicio_banca_API_Banco1_idx` ON `mydb`.`Pago_servicio_banca` (`API_Banco_idAPI` ASC) VISIBLE;
+CREATE INDEX `fk_Pago_servicio_banca_API_Banco1_idx` ON `mydb`.`Pago_servicio_banca` (`API_Banco_idAPI` ASC) ;
 
-CREATE INDEX `fk_Pago_servicio_banca_cuenta_asociada1_idx` ON `mydb`.`Pago_servicio_banca` (`cuenta_origen` ASC, `session_id` ASC) VISIBLE;
+CREATE INDEX `fk_Pago_servicio_banca_cuenta_asociada1_idx` ON `mydb`.`Pago_servicio_banca` (`cuenta_origen` ASC, `session_id` ASC) ;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Transferencia`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Transferencia` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Transferencia` (
-  `idTransferencia` INT NOT NULL,
+  `idTransferencia` INT NOT NULL AUTO_INCREMENT,
   `monto` DOUBLE NULL,
   `cuenta_destino` VARCHAR(45) NULL,
   `nota_credito` VARCHAR(45) NULL,
@@ -150,18 +156,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Transferencia` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Transferencia_API_Banco1_idx` ON `mydb`.`Transferencia` (`API_Banco_idAPI` ASC) VISIBLE;
+CREATE INDEX `fk_Transferencia_API_Banco1_idx` ON `mydb`.`Transferencia` (`API_Banco_idAPI` ASC) ;
 
-CREATE INDEX `fk_Transferencia_cuenta_asociada1_idx` ON `mydb`.`Transferencia` (`cuenta_origen` ASC, `session_id` ASC) VISIBLE;
+CREATE INDEX `fk_Transferencia_cuenta_asociada1_idx` ON `mydb`.`Transferencia` (`cuenta_origen` ASC, `session_id` ASC) ;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Servicio_afiliado`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Servicio_afiliado` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Servicio_afiliado` (
-  `idServicio` INT NOT NULL,
+  `idServicio` INT NOT NULL AUTO_INCREMENT,
   `NombreServicio` VARCHAR(45) NULL,
   PRIMARY KEY (`idServicio`))
 ENGINE = InnoDB;
@@ -170,10 +175,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Pago_servicio_diverso`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Pago_servicio_diverso` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Pago_servicio_diverso` (
-  `ID` INT NOT NULL,
+  `ID` INT NOT NULL AUTO_INCREMENT,
   `Monto` DOUBLE NULL,
   `Fecha_facturacion` DATE NULL,
   `Fecha_pago` VARCHAR(45) NULL,
@@ -200,20 +204,19 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Pago_servicio_diverso` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Pago_servicio_diverso_Servicios_afiliados1_idx` ON `mydb`.`Pago_servicio_diverso` (`Servicios_afiliados_idServicios` ASC) VISIBLE;
+CREATE INDEX `fk_Pago_servicio_diverso_Servicios_afiliados1_idx` ON `mydb`.`Pago_servicio_diverso` (`Servicios_afiliados_idServicios` ASC) ;
 
-CREATE INDEX `fk_Pago_servicio_diverso_API_Banco1_idx` ON `mydb`.`Pago_servicio_diverso` (`API_Banco_idAPI` ASC) VISIBLE;
+CREATE INDEX `fk_Pago_servicio_diverso_API_Banco1_idx` ON `mydb`.`Pago_servicio_diverso` (`API_Banco_idAPI` ASC) ;
 
-CREATE INDEX `fk_Pago_servicio_diverso_cuenta_asociada1_idx` ON `mydb`.`Pago_servicio_diverso` (`cuenta_origen` ASC, `session_id` ASC) VISIBLE;
+CREATE INDEX `fk_Pago_servicio_diverso_cuenta_asociada1_idx` ON `mydb`.`Pago_servicio_diverso` (`cuenta_origen` ASC, `session_id` ASC) ;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Consulta_Cheque`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Consulta_Cheque` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Consulta_Cheque` (
-  `idConsulta_Cheque` INT NOT NULL,
+  `idConsulta_Cheque` INT NOT NULL AUTO_INCREMENT,
   `API_Banco_Banco_idBanco` INT NOT NULL,
   `fecha_inicial` DATE NULL,
   `fecha_final` DATE NULL,
@@ -234,18 +237,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Consulta_Cheque` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Consulta_Cheque_API_Banco1_idx` ON `mydb`.`Consulta_Cheque` (`API_Banco_idAPI` ASC, `API_Banco_Banco_idBanco` ASC) VISIBLE;
+CREATE INDEX `fk_Consulta_Cheque_API_Banco1_idx` ON `mydb`.`Consulta_Cheque` (`API_Banco_idAPI` ASC, `API_Banco_Banco_idBanco` ASC) ;
 
-CREATE INDEX `fk_Consulta_Cheque_cuenta_asociada1_idx` ON `mydb`.`Consulta_Cheque` (`cuenta_origen` ASC, `session_id` ASC) VISIBLE;
+CREATE INDEX `fk_Consulta_Cheque_cuenta_asociada1_idx` ON `mydb`.`Consulta_Cheque` (`cuenta_origen` ASC, `session_id` ASC) ;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Consulta_saldo`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Consulta_saldo` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Consulta_saldo` (
-  `idConsulta_saldo` INT NOT NULL,
+  `idConsulta_saldo` INT NOT NULL AUTO_INCREMENT,
   `nombre_cuenta` VARCHAR(45) NULL,
   `saldo_disponible` DOUBLE NULL,
   `saldo_reserva` DOUBLE NULL,
@@ -268,18 +270,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Consulta_saldo` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Consulta_saldo_API_Banco1_idx` ON `mydb`.`Consulta_saldo` (`API_Banco_idAPI` ASC) VISIBLE;
+CREATE INDEX `fk_Consulta_saldo_API_Banco1_idx` ON `mydb`.`Consulta_saldo` (`API_Banco_idAPI` ASC) ;
 
-CREATE INDEX `fk_Consulta_saldo_cuenta_asociada1_idx` ON `mydb`.`Consulta_saldo` (`cuenta_origen` ASC, `session_id` ASC) VISIBLE;
+CREATE INDEX `fk_Consulta_saldo_cuenta_asociada1_idx` ON `mydb`.`Consulta_saldo` (`cuenta_origen` ASC, `session_id` ASC) ;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Consulta_estado_cuenta`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Consulta_estado_cuenta` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Consulta_estado_cuenta` (
-  `idConsulta_estado_cuenta` INT NOT NULL,
+  `idConsulta_estado_cuenta` INT NOT NULL AUTO_INCREMENT,
   `fecha_inicial` DATE NULL,
   `fecha_final` DATE NULL,
   `formato_salida` VARCHAR(45) NULL,
@@ -299,18 +300,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Consulta_estado_cuenta` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Consulta_estado_cuenta_API_Banco1_idx` ON `mydb`.`Consulta_estado_cuenta` (`API_Banco_idAPI` ASC) VISIBLE;
+CREATE INDEX `fk_Consulta_estado_cuenta_API_Banco1_idx` ON `mydb`.`Consulta_estado_cuenta` (`API_Banco_idAPI` ASC) ;
 
-CREATE INDEX `fk_Consulta_estado_cuenta_cuenta_asociada1_idx` ON `mydb`.`Consulta_estado_cuenta` (`cuenta_origen` ASC, `session_id` ASC) VISIBLE;
+CREATE INDEX `fk_Consulta_estado_cuenta_cuenta_asociada1_idx` ON `mydb`.`Consulta_estado_cuenta` (`cuenta_origen` ASC, `session_id` ASC) ;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`detalle_estado_cuenta`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`detalle_estado_cuenta` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`detalle_estado_cuenta` (
-  `correlativo` INT NOT NULL,
+  `correlativo` INT NOT NULL AUTO_INCREMENT,
   `id_consulta` INT NOT NULL,
   `fecha_transaccion` DATE NULL,
   `monto` DOUBLE NULL,
@@ -323,16 +323,15 @@ CREATE TABLE IF NOT EXISTS `mydb`.`detalle_estado_cuenta` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_detalle_estado_cuenta_Consulta_estado_cuenta1_idx` ON `mydb`.`detalle_estado_cuenta` (`id_consulta` ASC) VISIBLE;
+CREATE INDEX `fk_detalle_estado_cuenta_Consulta_estado_cuenta1_idx` ON `mydb`.`detalle_estado_cuenta` (`id_consulta` ASC) ;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`detalle_cheque`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`detalle_cheque` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`detalle_cheque` (
-  `correlativo` INT NOT NULL,
+  `correlativo` INT NOT NULL AUTO_INCREMENT,
   `id_consulta` INT NOT NULL,
   `numero_cheque` INT NULL,
   `fecha_cobro` DATE NULL,
@@ -346,9 +345,4 @@ CREATE TABLE IF NOT EXISTS `mydb`.`detalle_cheque` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_detalle_cheque_Consulta_Cheque1_idx` ON `mydb`.`detalle_cheque` (`id_consulta` ASC) VISIBLE;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE INDEX `fk_detalle_cheque_Consulta_Cheque1_idx` ON `mydb`.`detalle_cheque` (`id_consulta` ASC) ;
