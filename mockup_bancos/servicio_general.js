64fb7ebdbf48f04c812e038d45190ff95ade4bee
Pablo Daniel Villagrán <pdvillc@gmail.com>
2021-06-10 14:19:01 -0600

    Se agrega servicio para generación de token

--- mockup_bancos/servicio_general.js
+++ mockup_bancos/servicio_general.js
@@ -1,2 +1,18 @@
-const express = require('express');
+import express from 'express';
+import { randomBytes } from 'crypto';
 
+const app = express();
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+let puerto = '9000';
+let nombre = 'Servicio mockup';
+
+function get_token(req, res){
+    let token = randomBytes(64).toString('Base64');
+    res.json({success:true, token: token});
+}
+
+app.post('/login', (req, res) => {get_token(req,res)});
+
+app.listen(puerto, () => console.log(nombre +" @ port " + puerto));
