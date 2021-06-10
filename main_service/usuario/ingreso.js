af6c756cedd29fc2612b532390dbea567656caff
Pablo Daniel Villagrán <pdvillc@gmail.com>
2021-06-10 15:32:31 -0600

    Servicio de login funcional

---
+++ main_service/usuario/ingreso.js
@@ -0,0 +1,39 @@
+import service_prototype from '../service_prototype.js';
+import { inherits } from 'util';
+import mysql from 'mysql';
+import axios from 'axios';
+
+function Ingreso_service(){
+    Ingreso_service.apply(this, arguments);
+}
+
+inherits(Ingreso_service, service_prototype);
+
+var this_service = Ingreso_service.prototype;
+
+this_service.puerto = 'port.usuario.ingreso';
+this_service.nombre = 'Servicio LogIn';
+
+this_service.login = function(req, res){
+    let usuario = mysql.escape(req.body.usuario);//sanitiza ingresos de usuario
+    let correo = mysql.escape(req.body.correo);//sanitiza ingresos de usuario
+
+    //consumir web service de mockup
+    axios.post('http://localhost:9000/login',{"usuario": req.body.usuario, "password": req.body.password})
+    .then(serviceResponse => {
+        let token = mysql.escape(serviceResponse.data.token);
+        //Insertar transacción en tabla
+        this_service.query("INSERT INTO `sesion_usuario`(`Nombre_usuario`, `Token`, `fecha_hora_expira`, `correo_electrónico`, `API_Banco_idAPI`) VALUES ("+usuario+","+token+",date_add(now(), interval 30 MINUTE),"+correo+",'1')", res);
+        res.json({success:true, "token":serviceResponse.data.token});
+    })
+    .catch(error=>{
+        console.log(error);
+        res.json({success:false, error});
+    });
+}
+
+this_service.add_endpoint('POST', '/login/:banco', (req, res) => {this_service.login(req, res)});
+
+this_service.start_service();
+
+export default Ingreso_service;
