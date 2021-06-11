660947d1687956adc1059b5b346ea52ed91d6e38
Pablo Daniel Villagrán <pdvillc@gmail.com>
2021-06-10 21:55:03 -0600

    agregado servicio para generar detalle aleatorio

--- mockup_bancos/servicio_general.js
+++ mockup_bancos/servicio_general.js
@@ -13,6 +13,44 @@ function get_token(req, res){
     res.json({success:true, token: token});
 }
 
+function get_saldo(req, res){
+    console.log(req.body.fecha_ini, req.body.fecha_fin)
+    let detalle_aleatorio = generarRandom(req.body.fecha_ini, req.body.fecha_fin);
+    res.json({
+        success:true, 
+        numero_cuenta: req.numero_cuenta,
+        detalle:detalle_aleatorio
+    });
+}
+
+function generarRandom(start, end){
+    let s = start.split('-');
+    let e = end.split('-');
+    let s_date = new Date(s[0], s[1]-1, s[2]);
+    let e_date = new Date(e[0], e[1]-1, e[2]);
+
+    let arr = new Array();
+    for(let i = 0; i<Math.ceil(Math.random() * 6); i++){
+        arr.push({desc: getRandomDesc(), 
+            monto: Math.round(((Math.random() * - 1000) + 500)*100)/100,
+            fecha: getRandomDate(s_date, e_date)
+        });
+    }
+    return arr;
+}
+
+function getRandomDesc() {
+    let r = Math.floor(Math.random()*5);
+    let descs = ['Compras online','Wallmart','5B','Transferencia','McDonalds','Cheque'];
+    return descs[r];
+}
+
+//randomDate(new Date(2021, 0, 1), new Date(2021, 5, 1))
+function getRandomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}  
+
 app.post('/mockup/inicio_sesion', (req, res) => {get_token(req,res)});
+app.post('/mockup/c/saldo', (req, res) => {get_saldo(req,res)})
 
 app.listen(puerto, () => console.log(nombre +" @ port " + puerto));
